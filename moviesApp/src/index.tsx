c48461a84b11b6bd7a4ab29153e0a35e3d514e4d
Pavan Gurrala <pavankumarg987@gmail.com>
2025-05-06 17:25:08 +0100

    Excercise - Added Upcoming movies page

--- moviesApp/src/index.tsx
+++ moviesApp/src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "../src/pages/homePage"
 import MoviePage from "../src/pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
-
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -15,6 +15,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
